b9cfebb552bc82f9f5a68fb31b1478ee3a075d17
JaisonCode <jaisoncode@gmail.com>
2024-03-21 16:49:14 -0300

    logica de operacoes

---
+++ Calculadora/assets/js/script.js
@@ -0,0 +1,82 @@
+//zona de calculo - calculo feito
+const operationUsed = document.querySelector('#operation-used');
+const ResultCalc = document.querySelector('#operation-result');
+
+function updatePrevios() {
+    operationUsed.textContent = expressionUsed;
+    ResultCalc.textContent = resultado;
+}
+
+//botoes da calculadora
+const botoes = document.querySelectorAll('.btn');
+
+let expressionUsed = [];
+let resultado;
+let adicionar = [];
+let calculo = [];
+
+botoes.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if (button.value == 1 ||
+            button.value == 2 ||
+            button.value == 3 ||
+            button.value == 4 ||
+            button.value == 5 ||
+            button.value == 6 ||
+            button.value == 7 ||
+            button.value == 8 ||
+            button.value == 9 ||
+            button.value == 0) {
+            addCaracter(button.value);
+
+        } else if (button.value == "+" ||
+            button.value == "-" ||
+            button.value == "*" ||
+            button.value == "." ||
+            button.value == "/" ||
+            button.value == "%"
+        ) {
+            if (expressionUsed.length === 0 ||
+                expressionUsed.slice(-1) === "+" ||
+                expressionUsed.slice(-1) === "-" ||
+                expressionUsed.slice(-1) === "*" ||
+                expressionUsed.slice(-1) === "/" ||
+                expressionUsed.slice(-1) === ".") {
+            } else {
+                addCaracter(button.value);
+
+            }
+        } else if (button.value == "=") {
+            calulator();
+            updatePrevios();
+        } else if (button.value == "clean") {
+            expressionUsed = [];
+            updatePrevios();
+
+        } else {
+            expressionUsed = expressionUsed.slice(0, -1);
+        }
+        calulator()
+    });
+});
+
+function addCaracter(button) {
+    adicionar += button;
+    expressionUsed += adicionar;
+    adicionar = [];
+    updatePrevios();
+}
+
+function calulator() {
+    if (expressionUsed.slice(-1) === "+" ||
+        expressionUsed.slice(-1) === "-" ||
+        expressionUsed.slice(-1) === "*" ||
+        expressionUsed.slice(-1) === "/" ||
+        expressionUsed.slice(-1) === "." ||
+        expressionUsed.slice(-1) === "%") {
+    } else {
+        resultado = eval(expressionUsed);
+        updatePrevios();
+    }
+}
+
