c574d24949175840cfdb6de88cc3328c55045812
JaisonCode <jaisoncode@gmail.com>
2024-03-24 21:47:21 -0300

    add js com logica de geracao de senha + ajustes html/css

---
+++ Gerador-De-Senhas/script.js
@@ -0,0 +1,29 @@
+let ramdonChar = "";
+let password = "";
+console.log(ramdonChar)
+console.log(password)
+
+
+document.querySelector('#button-generator').addEventListener('click', function () {
+    let ramdonChar = "";
+    let password = "";
+    const length = document.querySelector('#length-char').value;
+
+    const upperCase = document.querySelector('#uppercase-char');
+    const lowerCase = document.querySelector('#lowercase-char');
+    const number = document.querySelector('#number');
+    const symbols = document.querySelector('#symbols');
+
+    
+    if (upperCase.checked) ramdonChar += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (lowerCase.checked) ramdonChar += 'abcdefghijklmnopqrstuvwxyz';
+    if (symbols.checked) ramdonChar += '!@#$%^&*()_+-=[]{}|;:,.<>?';
+    if (number.checked) ramdonChar += '0123456789';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * ramdonChar.length);
+        password += ramdonChar.charAt(randomIndex);
+    }
+    console.log(password);
+    document.querySelector('#password-content').innerHTML = password;
+});
